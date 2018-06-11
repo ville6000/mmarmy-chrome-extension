823605dfe1bd022f0b260cd46455c609ec49b457
Ville Viklund <ville.viklund@morgan.fi>
2018-06-11 14:33:35 +0300

    Add functionality for record by organization

--- main.js
+++ main.js
@@ -12,9 +12,23 @@ const init = () => {
   const breakdown = recordBreakdown(rows);
   container.appendChild(createRecordBreakdownMarkup(breakdown));
 
+  container.appendChild(
+    createRecordByOrganizationMarkup(recordByOrganization(rows))
+  );
+
   const recordTable = document.querySelectorAll(".record");
 
   document.querySelector(".middle .b").insertBefore(container, recordTable[0]);
+};
+
+/**
+ * Is current table row win or title win
+ *
+ * @param {Node} el Table row
+ */
+const isWin = el => {
+  const tdClasses = el.querySelector("td:first-child").classList;
+  return tdClasses.contains("win") || tdClasses.contains("winTitle");
 };
 
 /**
@@ -107,6 +121,50 @@ const createRecordBreakdownMarkup = breakdown => {
   const winDecision = document.createElement("div");
   winDecision.textContent = `Decision: ${breakdown.wins.decision}`;
   container.appendChild(winDecision);
+
+  return container;
+};
+
+/**
+ * Create record by organization object
+ * @param {NodeList} rows List of table rows
+ */
+const recordByOrganization = rows => {
+  const record = [];
+
+  rows.forEach((el, idx) => {
+    const key = isWin(el) ? "wins" : "losses";
+    const organization = el.querySelectorAll("td").item(7).textContent;
+
+    if (typeof record[organization] === "undefined") {
+      record[organization] = {
+        wins: 0,
+        losses: 0
+      };
+    }
+
+    record[organization][key]++;
+  });
+
+  return record;
+};
+
+/**
+ * Create markup for record by organization
+ * @param {object} record
+ */
+const createRecordByOrganizationMarkup = record => {
+  const container = document.createElement("div");
+  const title = document.createElement("h4");
+  title.textContent = "Record by Organization";
+  container.appendChild(title);
+
+  Object.keys(record).forEach(key => {
+    let item = document.createElement("div");
+    item.textContent = `${key}: ${record[key].wins} - ${record[key].losses}`;
+
+    container.appendChild(item);
+  });
 
   return container;
 };
